8779b04c1f50feec30dd80260daa17f4da452430
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-10-19 16:09:26 -0500

    add bcrypt librarie n admin scheme

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import Admin from '../schemas/admin.schema.js';
 
 const app = express();
 
@@ -12,6 +13,17 @@ app.use(cors());
 //routes
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('/testAdmin', (req, res) => {
+	Admin.create({
+		email: 'victor.sapuyes@correounivalle.edu.co',
+		password: 'unaclavesecreta',
+	}).then(admin => {
+		admin
+			.validPassword('unaclavesecreta', admin.password)
+			.then(value => res.json(value));
+	});
 });
 
 export default app;
---
+++ src/schemas/admin.schema.js
@@ -0,0 +1,52 @@
+import DataTypes from 'sequelize';
+import sequelize from '../config/db.js';
+import bcrypt from 'bcrypt';
+
+const Admin = sequelize.define(
+	'Admin',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		email: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		freezeTableName: true, // no change name
+		tableName: 'admins',
+		timestamps: false, // no updateat, no createate
+		hooks: {
+			beforeCreate: async admin => {
+				const salt = await bcrypt.genSaltSync(10, 'a');
+				admin.password = bcrypt.hashSync(admin.password, salt);
+			},
+			beforeUpdate: async admin => {
+				const salt = await bcrypt.genSaltSync(10, 'a');
+				admin.password = bcrypt.hashSync(admin.password, salt);
+			},
+		},
+		instanceMethods: {
+			validPassword: password => {
+				return bcrypt.compareSync(password, this.password);
+			},
+		},
+	}
+);
+
+Admin.prototype.validPassword = async (password, hash) => {
+	return await bcrypt.compareSync(password, hash);
+};
+
+export default Admin;
