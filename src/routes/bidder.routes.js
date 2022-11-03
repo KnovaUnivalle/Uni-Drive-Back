cfcc04a5affcc403bccc7db86ec749f36b5dc7c2
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-11-02 23:29:07 -0500

    auth dtos

--- src/routes/bidder.routes.js
+++ src/routes/bidder.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import bidderLoginController from '../controllers/bidder/login.controller.js';
+import bidderJWTDTO from '../dto/bidder/auth.dto.js';
 import bidderCreateDTO from '../dto/bidder/create.dto.js';
 import loginDTO from '../dto/login.dto.js';
 
 const bidderRouter = Router();
 
 bidderRouter.post('/login', loginDTO, bidderLoginController);
-bidderRouter.post('/create', bidderCreateDTO);
+bidderRouter.post('/create', bidderJWTDTO, bidderCreateDTO);
 
 export default bidderRouter;
--- src/routes/rider.routes.js
+++ src/routes/rider.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import riderLoginController from '../controllers/rider/login.controller.js';
 import loginDTO from '../dto/login.dto.js';
+import riderJWTDTO from '../dto/rider/auth.dto.js';
 import riderCreateDTO from '../dto/rider/create.dto.js';
 
 const riderRouter = Router();
 
 riderRouter.post('/login', loginDTO, riderLoginController);
-riderRouter.post('/create', riderCreateDTO);
+riderRouter.post('/create', riderJWTDTO, riderCreateDTO);
 
 export default riderRouter;
