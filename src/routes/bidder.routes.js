ca0b25187ac5f98120d8e77d20075109d2b7b290
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-07 14:29:38 -0500

    add route to get active vehiculo by bidder

--- src/routes/bidder.routes.js
+++ src/routes/bidder.routes.js
@@ -3,10 +3,19 @@ import bidderLoginController from '../controllers/bidder/login.controller.js';
 import bidderCreateController from '../controllers/bidder/create.controller.js';
 import bidderCreateDTO from '../dto/bidder/create.dto.js';
 import loginDTO from '../dto/login.dto.js';
+import authenticationController from '../controllers/authentication.controller.js';
+import authorizationController from '../controllers/bidder/authorization.controller.js';
+import { getVehicles } from '../controllers/vehicle/get.controller.js';
 
 const bidderRouter = Router();
 
 bidderRouter.post('/login', loginDTO, bidderLoginController);
 bidderRouter.post('/create', bidderCreateDTO, bidderCreateController);
+bidderRouter.get(
+	'/vehicles',
+	authenticationController,
+	authorizationController,
+	getVehicles
+);
 
 export default bidderRouter;
