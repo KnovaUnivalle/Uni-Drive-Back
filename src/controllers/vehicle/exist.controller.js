86ccea46bb0c50fb2c41ddf52bd62c9fc9ba6b1c
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2022-12-08 16:18:51 -0500

    create exist controllers

---
+++ src/controllers/vehicle/exist.controller.js
@@ -0,0 +1,22 @@
+import Vehicle from '../../schemas/vehicle.schema.js';
+
+/**
+ * Check if vehicle exist
+ * @param {Object} req
+ * @param {Object} res
+ * @returns status and message
+ */
+const bidderCreateController = async (req, res, next) => {
+	const { plate } = req.body;
+
+	//validate duplicate plate
+	const vehicleByPlate = await Vehicle.findOne({ where: { plate: plate } });
+	if (vehicleByPlate)
+		return res.status(409).send({
+			errors: ['Ya existe un vehiculo registrado con esa placa'],
+		});
+
+	next();
+};
+
+export default bidderCreateController;
