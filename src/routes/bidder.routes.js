d4a36afc57f9ee760db5aacfc9607a6a27bf899a
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2022-12-08 16:26:07 -0500

    update routes

--- src/routes/bidder.routes.js
+++ src/routes/bidder.routes.js
@@ -6,11 +6,21 @@ import loginDTO from '../dto/login.dto.js';
 import authenticationController from '../controllers/authentication.controller.js';
 import authorizationController from '../controllers/bidder/authorization.controller.js';
 import { getVehicles } from '../controllers/vehicle/get.controller.js';
+import bidderExistController from '../controllers/bidder/exist.controller.js';
+import vehicleExistController from '../controllers/vehicle/exist.controller.js';
+import vehicleCreateController from '../controllers/vehicle/create.controller.js';
 
 const bidderRouter = Router();
 
 bidderRouter.post('/login', loginDTO, bidderLoginController);
-bidderRouter.post('/create', bidderCreateDTO, bidderCreateController);
+bidderRouter.post(
+	'/create',
+	bidderCreateDTO,
+	bidderExistController,
+	vehicleExistController,
+	bidderCreateController,
+	vehicleCreateController
+);
 bidderRouter.get(
 	'/vehicles',
 	authenticationController,
