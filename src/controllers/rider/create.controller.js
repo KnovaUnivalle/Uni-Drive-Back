d71b8bb74f86eccd4ee075f9d98637fc43f34f53
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2022-12-08 16:19:44 -0500

    update create controllers

--- src/controllers/rider/create.controller.js
+++ src/controllers/rider/create.controller.js
@@ -1,7 +1,7 @@
 import Rider from '../../schemas/rider.schema.js';
 
 /**
- * Create a rider in database if not exist
+ * Create a rider in database
  * @param {Object} req
  * @param {Object} res
  * @returns status and message
@@ -18,22 +18,6 @@ const riderCreateController = async (req, res) => {
 		city,
 	} = req.body;
 
-	//validate duplicate email
-	const riderByEmail = await Rider.findOne({ where: { email: email } });
-	if (riderByEmail)
-		return res.status(409).send({
-			errors: ['Ya existe un usuario con ese email registrado'],
-		});
-
-	//validate duplicate cc
-	const riderByDocument = await Rider.findOne({
-		where: { document: document },
-	});
-	if (riderByDocument)
-		return res.status(409).send({
-			errors: ['Ya existe un usuario con ese documento registrado'],
-		});
-
 	//create rider and send status
 	await Rider.create({
 		firstName: firstName,
@@ -46,7 +30,7 @@ const riderCreateController = async (req, res) => {
 		CityID: city,
 	});
 
-	return res.status(201).send('Pasajero registrado con éxito');
+	return res.status(201).send('Registrado con éxito');
 };
 
 export default riderCreateController;
