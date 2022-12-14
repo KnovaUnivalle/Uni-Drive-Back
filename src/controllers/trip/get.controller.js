d02c328f00c786b271870587ceb3fbbf148270f9
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2022-12-13 20:41:27 -0500

    get controller trip

---
+++ src/controllers/trip/get.controller.js
@@ -0,0 +1,27 @@
+import Trip from '../../schemas/trip.schema.js';
+
+/**
+ * Send trip { id, date, day, rate, description, toUniversity, meetPoint, active} actives from database
+ * @param {Object} req
+ * @param {Object} res
+ * @returns status and data
+ */
+export const getTrips = async (req, res) => {
+	const { id } = req;
+
+	const data = await Trip.findAll({
+		where: { BidderId: id },
+		attributes: [
+			'id',
+			'date',
+			'day',
+			'rate',
+			'description',
+			'toUniversity',
+			'meetPoint',
+			'active',
+		],
+	});
+
+	return res.status(200).json(data);
+};
