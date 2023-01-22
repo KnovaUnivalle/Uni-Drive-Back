acecfd65cd80a549fc1ed47109c810faa739e9a9
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-22 02:21:26 -0500

    add getVehicles and change name controller

--- src/controllers/vehicle/get.controller.js
+++ src/controllers/vehicle/get.controller.js
@@ -1,3 +1,4 @@
+import Bidder from '../../schemas/bidder.schema.js';
 import BrandVehicle from '../../schemas/brandVehicle.schema.js';
 import TypeVehicle from '../../schemas/typeVehicle.schema.js';
 import Vehicle from '../../schemas/vehicle.schema.js';
@@ -8,7 +9,7 @@ import Vehicle from '../../schemas/vehicle.schema.js';
  * @param {Object} res
  * @returns status and data
  */
-export const getVehicles = async (req, res) => {
+export const getActivesVehiclesBidder = async (req, res) => {
 	const { id } = req;
 
 	const data = await Vehicle.findAll({
@@ -18,6 +19,21 @@ export const getVehicles = async (req, res) => {
 			{ model: TypeVehicle, attributes: ['description'] },
 			{ model: BrandVehicle, attributes: ['description'] },
 		],
+	});
+
+	return res.status(200).json(data);
+};
+
+/**
+ * Send vehicle { id, plate, available, type, brand, bidder } actives from database
+ * @param {Object} req
+ * @param {Object} res
+ * @returns status and data
+ */
+export const getVehicles = async (req, res) => {
+	const data = await Vehicle.findAll({
+		attributes: ['id', 'plate', 'available'],
+		include: [{ model: Bidder, attributes: ['id', 'firstName'] }],
 	});
 
 	return res.status(200).json(data);
--- src/routes/vehicle.routes.js
+++ src/routes/vehicle.routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import authorizationAdminController from '../controllers/admin/authorization.controller.js';
+import authenticationController from '../controllers/authentication.controller.js';
+import { getVehicles } from '../controllers/vehicle/get.controller.js';
 
 const vehicleRouter = Router();
+
+vehicleRouter.get(
+	'/',
+	authenticationController,
+	authorizationAdminController,
+	getVehicles
+);
 
 export default vehicleRouter;
