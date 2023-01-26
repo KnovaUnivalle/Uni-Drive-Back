e6fb0313ddbd9a8b0a085cab7182f1739ce0684b
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-26 11:14:19 -0500

    route

--- src/routes/rider.routes.js
+++ src/routes/rider.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import authenticationController from '../controllers/authentication.controller.js';
 import authorizationRiderController from '../controllers/rider/authorization.controller.js';
+import { createRiderInTripController } from '../controllers/trip/create.controller.js';
 import { getTripRiderController } from '../controllers/trip/get.controller.js';
 import { searchTripRiderController } from '../controllers/trip/search.controller.js';
 
@@ -9,5 +10,6 @@ const riderRouter = Router();
 riderRouter.use(authenticationController, authorizationRiderController);
 riderRouter.get('/trips/search', searchTripRiderController);
 riderRouter.get('/trips', getTripRiderController);
+riderRouter.post('/trips/create', createRiderInTripController);
 
 export default riderRouter;
