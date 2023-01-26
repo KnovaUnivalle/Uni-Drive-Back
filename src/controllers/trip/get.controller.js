0cfacb0552acb8031f36ce4544352cfe8d3456d5
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2023-01-26 11:00:30 -0500

    add get rider controller

--- src/controllers/trip/get.controller.js
+++ src/controllers/trip/get.controller.js
@@ -1,4 +1,6 @@
 import Bidder from '../../schemas/bidder.schema.js';
+import Rider from '../../schemas/rider.schema.js';
+import RiderInTrip from '../../schemas/riderInTrip.schema.js';
 import Trip from '../../schemas/trip.schema.js';
 import Vehicle from '../../schemas/vehicle.schema.js';
 import { arrayObjectsToArrayIds } from '../../utils/arrayMethods.js';
@@ -35,6 +37,40 @@ export const getTripsController = async (req, res) => {
 			'active',
 		],
 		include: [{ model: Vehicle, attributes: ['plate', 'TypeVehicleId'] }],
+	});
+
+	return res.status(200).json(data);
+};
+
+export const getTripRiderController = async (req, res) => {
+	const { id } = req;
+	const { active } = req.query;
+
+	const queryActive = active || true;
+
+	const trips = await RiderInTrip.findAll({
+		where: { RiderId: id },
+		attributes: ['TripId'],
+	});
+
+	const ids = arrayObjectsToArrayIds(trips);
+
+	const data = await Trip.findAll({
+		where: { active: queryActive, id: ids },
+		attributes: [
+			'id',
+			'date',
+			'day',
+			'rate',
+			'description',
+			'toUniversity',
+			'meetPoint',
+			'active',
+		],
+		include: [
+			{ model: Vehicle, attributes: ['plate', 'TypeVehicleId'] },
+			{ model: Bidder, attributes: ['firstName', 'lastName', 'numberPhone'] },
+		],
 	});
 
 	return res.status(200).json(data);
--- src/routes/rider.routes.js
+++ src/routes/rider.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import authenticationController from '../controllers/authentication.controller.js';
 import authorizationRiderController from '../controllers/rider/authorization.controller.js';
+import { getTripRiderController } from '../controllers/trip/get.controller.js';
 import { searchTripRiderController } from '../controllers/trip/search.controller.js';
 
 const riderRouter = Router();
 
 riderRouter.use(authenticationController, authorizationRiderController);
-
 riderRouter.get('/trips/search', searchTripRiderController);
+riderRouter.get('/trips', getTripRiderController);
 
 export default riderRouter;
